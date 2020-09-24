10ab61bce8a6120a41fdb36a0a58e52b74306f3a
Marcos Guilherme <mguilherme.nit@gmail.com>
2020-09-24 10:58:44 -0300

    Dao Factory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Seller;
+
 public interface SellerDao {
+	
+	void insert(Seller obj);
+	void update(Seller Obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id); //retornando um department recebendo integer id
+	List<Seller> findAll();
+	
 
 }
